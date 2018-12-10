b83c32e458f7253dd8a5a216f463865fa55b39ed
root <root@nf-server13.nf.cl.cam.ac.uk>
2018-12-10 17:29:37 +0000

    Exanic setup script is actually robust now

--- setup/setup_exanic_machine.sh
+++ setup/setup_exanic_machine.sh
@@ -3,4 +3,12 @@
 mkdir -p ~/jcw78
 
 pushd ~/jcw78
-git clone https://github.com/exablaze-oss/exanic-software
+if [[ ! -d exanic-software ]]; then
+	git clone https://github.com/exablaze-oss/exanic-software
+fi
+
+# Regardless, check out the 'right' version and build it all.
+pushd exanic-software
+git checkout c2aff3c0d120d4cf278b32c1c0b37b45904cb788
+make
+popd
