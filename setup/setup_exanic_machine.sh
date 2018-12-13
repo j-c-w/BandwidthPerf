a0b10b155ea5e435513bca403181dd079fa69bf9
root <root@nf-server13.nf.cl.cam.ac.uk>
2018-12-13 19:41:11 +0000

    Add some code to load the appropriate kernel modules and firmware

--- setup/setup_exanic_machine.sh
+++ setup/setup_exanic_machine.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+set -ue
 mkdir -p /root/jcw78
 
 pushd /root/jcw78
@@ -11,4 +12,42 @@ fi
 pushd exanic-software
 git checkout c2aff3c0d120d4cf278b32c1c0b37b45904cb788
 make
+make install
 popd
+
+# Once we have this, we can load the right kernel module:
+if [[ $(lsmod | grep -ce 'exasock') -ge 1 ]]; then
+	rmmod exasock
+fi
+if [[ $(lsmod | grep -ce 'exanic') -ge 1 ]]; then
+	rmmod exanic
+fi
+
+modprobe exasock 
+modprobe exanic
+exanic-config exanic0
+
+# Check that the firmware is the right date:
+pushd /root/jcw78/exanic-software/util
+fm_date=$(./exanic-config | grep -ce 'Firmware date: 20180221 ')
+popd
+if [[ $fm_date == 0 ]]; then
+	# Now, get the firmware and install it.
+	if [[ ! -f exanic_hpt_20180221.fw.gz ]]; then
+		wget https://exablaze.com/downloads/exanic/exanic_hpt_20180221.fw.gz
+	fi
+
+	if [[ ! -f exanic_hpt_20180221.fw ]]; then
+		gzip -d exanic_hpt_20180221.fw.gz
+	fi
+
+	# Now, install that firmware.
+	firmware_loc="$PWD/exanic_hpt_20180221.fw"
+	pushd /root/jcw78/exanic-software/util
+	./exanic-fwupdate -d exanic0 -r $firmware_loc
+	# Note that the system needs a reboot now, so
+	# exit with an error, inform the user and then wait.
+	echo "$(hostname) NOW NEEDS TO BE REBOOTED.  Re-run this script after reboot."
+	# Exit with an error to make sure all scripts stop.
+	exit 1
+fi
