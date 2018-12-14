b13a1c7b73ce24becfa44c6016b2d6776a027fb3
root <root@nf-server13.nf.cl.cam.ac.uk>
2018-12-14 10:20:56 +0000

    Take a size as argument

--- HPT_test/run_one_port_capture.sh
+++ HPT_test/run_one_port_capture.sh
@@ -4,6 +4,7 @@
 # of the HPT.  It runs for a few seconds.
 
 set -eu
+zmodload zsh/mathfunc
 
 if [[ "$#" -ne 1 ]]; then
 	echo "Usage: $0 <rate (in mbps)>"
@@ -11,6 +12,7 @@ if [[ "$#" -ne 1 ]]; then
 fi
 
 typeset -i rate=$1
+typeset -i size=$2
 
 if [[ $rate -gt 10000 ]]; then
 	echo "Rate cannot be greater than 10000."
@@ -24,9 +26,9 @@ storage_location=$(get_config_value "HPTTempStorageLocation")
 target_time=10
 # We are using a 10gig link and 64 byte packets.  So, the wire
 # time is 64 * 8 / 10000* 10**6 \approx 52ns.
-ipg=$(echo "scale=30; (64 * 8 / $rate) * 1000000" | bc)
+ipg=$((int(rint($(echo "scale=30; (64 * 8 / $rate) * 1000" | bc)))))
 echo "c"
-num_packets=$(echo "scale=30; $target_time * 1000000000 / ($ipg + 53)" | bc | bc)
+num_packets=$((int(rint($(echo "scale=30; $target_time * 1000000000 / ($ipg + 53)" | bc)))))
 capfile=hpt_at_${rate}_mbps
 
 echo "IPG: $ipg"
@@ -34,13 +36,13 @@ echo "Packets $num_packets"
 
 ./HPT_end_capture.sh
 echo "Killed all other running HPT capture instances."
-./HPT_start_capture.sh 0 $storage_location/$capfile
+./HPT_start_capture.sh "${size}B" $storage_location/$capfile
 echo "Capture started"
-./OSNT_cmd.sh -ifp0 ../sample_traces/64.cap
+./OSNT_cmd.sh -ifp0 /root/jcw78/pcap_files/$size.cap
 echo "Loaded PCAP files"
 sleep 1
 ./OSNT_cmd.sh -rpn0 $num_packets -ipg0 $ipg -run
-echo "Capture started!"
+echo "Generation started!"
 sleep 12
 ./HPT_end_capture.sh
 
