bc933e0d7f4a2dd469e216975b7eaed464e2df5d
root <root@nf-server13.nf.cl.cam.ac.uk>
2018-12-14 10:19:37 +0000

    Update the local commands

--- HPT_test/local_HPT_move.sh
+++ HPT_test/local_HPT_move.sh
@@ -1,4 +1,18 @@
 #!/bin/echo "This should not be run directly."
-
 echo "On machine $(hostname)"
+set -eu
 
+if [[ "$#" -ne 2 ]]; then
+	echo "Usage: source file, target file"
+	echo "Actual usage was $@ ($#)"
+	exit 1
+fi
+
+# We attempt to make the target directory if it does not
+# exist.
+
+if [[ ! -d $(dirname $2) ]]; then
+	mkdir -p $(dirname $2)
+fi
+
+mv $1 $2
--- HPT_test/local_HPT_start_capture.sh
+++ HPT_test/local_HPT_start_capture.sh
@@ -8,7 +8,6 @@ fi
 
 port=$1
 out_file=$2
-set -x
 
 # The ExaNIC appears to react fine to having multiple
 # instances running.  There is a kill script, but
@@ -16,5 +15,5 @@ set -x
 # experiments on the machine.
 pushd ~/jcw78/exanic-software/util
 echo "Recording on port $port into file $out_file"
-nohup ./exanic-capture -i exanic0:$port -w "$out_file" &
+nohup ./exanic-capture -i exanic0:$port -w "$out_file" &> err_port$port -F erf &
 echo "Capture started!"
