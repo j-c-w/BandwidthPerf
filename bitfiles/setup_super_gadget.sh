f6d87f3cfb9f365fba63bf9f22293b70e4c6f277
root <root@nf-server13.nf.cl.cam.ac.uk>
2019-01-26 18:10:26 +0000

    Fix the setup_super_gadget script to set the right enviroment variables

--- bitfiles/setup_super_gadget.sh
+++ bitfiles/setup_super_gadget.sh
@@ -2,8 +2,10 @@
 
 export DRIVER_FOLDER=/root/jcw78/NetFPGA-SUME-live/lib/sw/std/driver/sume_riffa_v1_0_0
 export SUME_FOLDER=/root/jcw78/NetFPGA-SUME-live
+export PATH=$PATH:/home/SDK/2016.4/bin/:/home/Vivado/2016.4/bin/
 
 current_dir=$PWD
 pushd NetFPGA-SUME-live/tools/scripts
+chmod +x ./run_load_image.sh
 ./run_load_image.sh $current_dir/setup_super_gadget.sh
 popd
