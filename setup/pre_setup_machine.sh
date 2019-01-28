04731e9e611fe220ecffeeccbc43f7d44d29f654
root <root@nf-server13.nf.cl.cam.ac.uk>
2019-01-28 14:47:42 +0000

    Add s setup_machine that can be run on a local machine without the scripts repo existing yet.

---
+++ setup/pre_setup_machine.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# This script does a pre-setup to make sure that the setup scripts exist etc.
+
+mkdir -p /root/jcw78
+if [[ ! -d /root/jcw78/scripts ]]; then
+	git clone https://github.com/j-c-w/BandwidthPerf scripts
+fi
+
+echo "Pre setup done!"
+echo "Calling setup machine!"
+cd /root/jcw78/scripts/setup/
+./setup_machine.sh
