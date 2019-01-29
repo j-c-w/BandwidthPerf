862c75c986723f7cac4127fe7c1a31cbefdd1cd0
root <root@nf-server13.nf.cl.cam.ac.uk>
2019-01-29 10:49:36 +0000

    Add script to start recording at a port

---
+++ hpt/record_port.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+if [[ $# -ne 4 ]]; then
+	echo "Usage: <port> <output capture file> <cpus list (see exanic documentation)> <command line output file>"
+	echo "The CPUs should be a colon-separated list of three CPUs. e.g. 44:45:46"
+	exit 1
+fi
+
+# The ExaNIC appears to react fine to having multiple               
+# instances running.  There is a kill script, but
+# we don't do that here.  This lets us start multiple
+# experiments on the machine.
+port=$1
+out_file=$2
+cpus=$3
+cmd_out_file=$4
+
+pushd /root/jcw78/scripts/hpt_setup/exanic-exact/exact-capture-1.0RC/bin
+nohup ./exact-capture -i $port -o $out_file --cpus 44:45:46 --no-kernel &> $cmd_out_file &
+echo "Capture started!"
