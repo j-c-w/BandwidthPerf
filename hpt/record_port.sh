5b7f5835fdf49f616c9e97647f0ddfe85eea8235
root <root@nf-server13.nf.cl.cam.ac.uk>
2019-01-30 08:38:46 +0000

    Record port makes sure the appropriate directories exist

--- hpt/record_port.sh
+++ hpt/record_port.sh
@@ -15,6 +15,10 @@ out_file=$2
 cpus=$3
 cmd_out_file=$4
 
+# Make sure that the appropriate folders exist:
+mkdir -p $(dirname $out_file)
+mkdir -p $(dirname $cmd_out_file)
+
 pushd /root/jcw78/scripts/hpt_setup/exanic-exact/exact-capture-1.0RC/bin
 nohup ./exact-capture -i $port -o $out_file --cpus 44:45:46 --no-kernel &> $cmd_out_file &
 echo "Capture started!"
