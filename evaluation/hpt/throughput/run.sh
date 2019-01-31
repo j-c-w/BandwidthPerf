466747ce1ff1acfede2691494caeead08d100578
root <root@nf-server13.nf.cl.cam.ac.uk>
2019-01-31 15:37:15 +0000

    Fix the throughput run

--- evaluation/hpt/throughput/run.sh
+++ evaluation/hpt/throughput/run.sh
@@ -10,6 +10,7 @@ if [[ $(dirname $0) != "." ]]; then
 fi
 
 runs=$(get_config_value Runs)
+HPTMachine=$(get_config_value MachineB)
 
 # First, setup the machines.
 ./setup_throuput_machines.sh
@@ -20,34 +21,36 @@ echo "Make sure port 0 of OSNT is connected to the HPT card."
 cont_msg="Enter> to continue when this is done"
 vared 'cont_msg'
 pushd one_port_scan/
-./run.sh
+# ./run.sh
 popd
 echo "Finished running rate scan on a single port."
 echo "Make sure wires are now configured so that port 0 of OSNT is connected through an optical tap to ports 0 and 1 of the HPT card"
+cont_msg="Enter> to continue when this is done"
+vared 'cont_msg'
 pushd two_port_scan/
 # These build up data accross runs.
-echo -n "" > /root/jcw78/nvme/port0_data_builder
-echo -n "" > /root/jcw78/nvme/port1_data_builder
-for i in ${1..$runs}; do 
+remote_run_command $HPTMachine "echo -n '' > /root/jcw78/nvme/port0_data_builder"
+remote_run_command $HPTMachine "echo -n '' > /root/jcw78/nvme/port1_data_builder"
+for i in $(seq 1 $runs); do 
 	./run.sh
 	# We need to get the data out and keep it in a unified
 	# file.
-	remote_run_script $PWD/plot.sh
+	remote_run_script $HPTMachine evaluation/hpt/throughput/two_port_scan/plot.sh /root/jcw78/nvme/two_port_scan
 	# Get the data files out:
-	remote_run_script general/paste_wrapper.sh /root/jcw78/nvme/two_port_scan/port0_data /root/jcw78/nvme/port0_data_builder /root/jcw78/nvme/port0_data_builder
-	remote_run_script general/paste_wrapper.sh /root/jcw78/nvme/two_port_scan/port1_data /root/jcw78/nvme/port1_data_builder /root/jcw78/nvme/port1_data_builder
+	remote_run_script $HPTMachine general/paste_wrapper.sh /root/jcw78/nvme/two_port_scan/port0_data /root/jcw78/nvme/port0_data_builder /root/jcw78/nvme/port0_data_builder
+	remote_run_script $HPTMachine general/paste_wrapper.sh /root/jcw78/nvme/two_port_scan/port1_data /root/jcw78/nvme/port1_data_builder /root/jcw78/nvme/port1_data_builder
 
 	# Archive the folders:
-	remote_run_script general/archive_results.sh /root/jcw78/nvme/two_port_scan/ /root/jcw78/nvme/two_port_scan_${i}.tar.bz2
+	remote_run_script $HPTMachine general/archive_results.sh /root/jcw78/nvme/two_port_scan/ /root/jcw78/nvme/two_port_scan_${i}.tar.bz2
 done
 
 # Run the analyses on the overall folders.
-remote_run_script $PWD/plot.sh /root/jcw78/nvme/port0_data_builder /root/jcw78/nvme/port1_data_builder
+remote_run_script $HPTMachine $PWD/plot.sh /root/jcw78/nvme/port0_data_builder /root/jcw78/nvme/port1_data_builder
 
 # Finally, move all the archived folders over to the LTS.
 lts_dir=$(get_config_value LTSLocations)
-remote_run_command "mv /root/jcw78/nvme/two_port_scan_* $lts_dir"
-remote_run_command "mv /root/jcw78/nvme/port{0,1}_data_builder $lts_dir"
+remote_run_command $HPTMachine "mv /root/jcw78/nvme/two_port_scan_* $lts_dir"
+remote_run_command $HPTMachine "mv /root/jcw78/nvme/port{0,1}_data_builder $lts_dir"
 
 popd
 echo "All done!"
