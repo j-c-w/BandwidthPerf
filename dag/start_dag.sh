d45c373fb8a0f13355e320042563b9ca129c2912
root <root@nf-server13.nf.cl.cam.ac.uk>
2019-02-08 14:12:55 +0000

    start_dag now makes the directories for the  output files it createss

--- dag/start_dag.sh
+++ dag/start_dag.sh
@@ -4,5 +4,13 @@ if [[ $# -ne 3 ]]; then
 	echo "Usage: $0 <device> <output name> <cmd output file>"
 fi
 
+if [[ ! -d $(dirname $2) ]]; then
+	mkdir -p $(dirname $2)
+fi
+
+if [[ ! -d $(dirname $3) ]]; then
+	mkdir -p $(dirname $3)
+fi
+
 dagload
 dagsnap -d $1 -o $2 > $3 &
