b1bd190acb40a522cc7af943d815f242955fffb3
root <root@nf-server13.nf.cl.cam.ac.uk>
2019-02-09 13:29:03 +0000

    Add a script to run an iperf test

---
+++ iperf/run_iperf.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+set -eu
+
+if [[ $# -ne 6 ]]; then
+	echo "Usage $0 <server> <server test interface> <client> <time> <server file> <client file>"
+	exit 1
+fi
+
+server=$1
+server_test_interface=$2
+client=$3
+time=$4
+server_file=$5
+client_file=$6
+
+source /root/jcw78/scripts/general/remote_run.sh
+
+kill_iperf() {
+	# Kill any existing iperfs.
+	remote_run_command $server "pkill iperf -9"
+	remote_run_command $client "pkill iperf -9"
+}
+kill_iperf
+
+# Start the server:
+remote_run_command $server "iperf -s > $server_file &" &
+remote_run_command $client "iperf -c $server_test_interface -t $time > $client_file &" &
+
+sleep $time
+
+# Now kill those servers:
+kill_iperf
+
+# Finally, get the server file and the client file back on the managing machine.
+scp $server:$server_file .
+scp $client:$client_file .
