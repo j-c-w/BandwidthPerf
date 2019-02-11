0b56890dcfa7a79be1ef8cf7bcd91da2bff669ee
Marcin Wojcik <marcin.wojcik@cl.cam.ac.uk>
2019-02-11 17:26:16 +0000

    The setup file now takes into account the install location for Vivado and the like

--- bitfiles/setup_bitfile.sh
+++ bitfiles/setup_bitfile.sh
@@ -5,9 +5,16 @@ if [[ $# -ne 1 ]]; then
 	exit 1
 fi
 
+set -eu
+
+source /root/jcw78/scripts/general/parse_config.sh
+
+SDKLocation=$(get_config_value VivadoLocation ../setup/config)
+XMDLocation=$(get_config_value XMDLocation ../setup/config)
+
 export DRIVER_FOLDER=/root/jcw78/NetFPGA-SUME-live/lib/sw/std/driver/sume_riffa_v1_0_0
 export SUME_FOLDER=/root/jcw78/NetFPGA-SUME-live
-export PATH=$PATH:/home/SDK/2016.4/bin/:/home/Vivado/2016.4/bin/
+export PATH=$PATH:$XMDLocation:$SDKLocation
 
 current_dir=$PWD
 pushd /root/jcw78/NetFPGA-SUME-live/tools/scripts
