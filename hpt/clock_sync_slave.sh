c031e9781228f540d53870eb89c262018bba6bf5
root <root@nf-server14.nf.cl.cam.ac.uk>
2019-02-11 04:33:24 +0000

    Fix issues with the clock sync not being disabled on the slave

--- hpt/clock_sync_slave.sh
+++ hpt/clock_sync_slave.sh
@@ -8,5 +8,9 @@ fi
 pkill exanic-clock-sync
 
 pushd /root/jcw78/scripts/hpt_setup/exanic-software/util/
+# Make sure that the PPS master pulse is off
+./exanic-config $1 pps-out off
+
+# Then start the slave.
 ./exanic-clock-sync $1:pps &
 popd
