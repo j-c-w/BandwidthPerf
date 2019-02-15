fa4f99c40151f9e36a20eeec8026d17220ff7cfa
dreamjhhan <jong.han@cl.cam.ac.uk>
2019-02-15 14:35:58 +0000

    Fix issues with missing sed command

--- apps/replacement_sed.sh
+++ apps/replacement_sed.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
+set -x
 
 if [[ $# -ne 1 ]]; then
 	echo "Usage $0 <NIC>"
 fi
-sed -i "/export DEFAULT_NIC='changeme'/export DEFAULT_NIC=${1}" ~/benchmark/benchmark/config-env.sh
+sed -i "s/export DEFAULT_NIC='changeme'/export DEFAULT_NIC=${1}/" ~/benchmark/benchmark/config-env.sh
