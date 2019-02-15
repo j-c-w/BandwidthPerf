5689f37d4d3e32c9927f492f9569a5a344826945
root <root@nf-server13.nf.cl.cam.ac.uk>
2019-02-15 14:48:26 +0000

    Fix the remote installation of apps

--- apps/install_apps.sh
+++ apps/install_apps.sh
@@ -1,6 +1,10 @@
-#!/bin/bash
+#!/bin/zsh
 
 # Install the apps folder on this device in the appropriate location on the machines specified.
+set -eu
+
+source /root/jcw78/scripts/general/parse_config.sh
+source /root/jcw78/scripts/general/remote_run.sh
 
 if [[ $# -eq 0 ]]; then
 	echo "Usage: $0 <machines to install>"
@@ -9,4 +13,6 @@ fi
 
 for arg in "$@"; do
 	scp -r /root/jcw78/SUMMER2017/apps/benchmark/ $arg:~/benchmark
+	default_nic=$(get_config_value $arg)
+	remote_run_script "$arg" apps/replacement_sed.sh "$default_nic"
 done
