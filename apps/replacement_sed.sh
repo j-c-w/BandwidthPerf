c67711cf180153206dedd157b3c01b160082950f
dreamjhhan <jong.han@cl.cam.ac.uk>
2019-02-15 15:42:50 +0000

    Fix pathname

--- apps/replacement_sed.sh
+++ apps/replacement_sed.sh
@@ -4,4 +4,4 @@ set -x
 if [[ $# -ne 1 ]]; then
 	echo "Usage $0 <NIC>"
 fi
-sed -i "s/export DEFAULT_NIC='changeme'/export DEFAULT_NIC=${1}/" ~/benchmark/benchmark/config-env.sh
+sed -i "s/export DEFAULT_NIC='changeme'/export DEFAULT_NIC=${1}/" ~/benchmark/config-env.sh
