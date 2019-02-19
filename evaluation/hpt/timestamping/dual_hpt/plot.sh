9ef6b13ef3f261ea9158c1b74b6b5d44a409c63b
root <root@nf-server13.nf.cl.cam.ac.uk>
2019-02-19 11:47:49 +0000

    Add plotting scripts for the dual-hpt test

---
+++ evaluation/hpt/timestamping/dual_hpt/plot.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Do a different plot for each run.
+set -eu
+
+source /root/jcw78/scripts/general/parse_config.sh
+
+lts_location=$(get_config_value LTSLocations ../config)
+tmp_location=$(get_config_value WorkDirectory ../config)
+runs=$(get_config_value Runs ../config)
+
+parallel "./single_plot.sh {} $lts_location $tmp_location" ::: $(seq 1 $runs)
---
+++ evaluation/hpt/timestamping/dual_hpt/single_plot.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+set -eu
+
+if [[ $# -ne 3 ]]; then
+	echo "Usage $0 <run no> <lts loc> <decompression location>"
+	exit 1
+fi
+run=$1
+
+# Move the compressed file to the decompression location
+cp $2/hpt_accuracy_different_cards_card_0/hpt_accuracy_different_cards_card_0_run_$run/10000_joint_timing_card_0-0.expcap.bz2 $3/different_cards_run_${run}_card_0.expcap.bz2
+cp $2/hpt_accuracy_different_cards_card_1/hpt_accuracy_different_cards_card_1_run_$run/10000_joint_timing_card_1-0.expcap.bz2 $3/different_cards_run_${run}_card_1.expcap.bz2
+echo "Done copying!"
+
+
+# Now decompress both:
+bzip2 -d $3/different_cards_run_${run}_card_0.expcap.bz2 &
+bzip2 -d $3/different_cards_run_${run}_card_1.expcap.bz2 &
+wait
+echo "Done decompressing!"
+
+# Now convert those to pcap:
+/root/jcw78/scripts/general/expcap_to_csv.sh $3/different_cards_run_${run}_card_0.expcap $3/different_cards_run_${run}_card_0.csv &
+/root/jcw78/scripts/general/expcap_to_csv.sh $3/different_cards_run_${run}_card_1.expcap $3/different_cards_run_${run}_card_1.csv &
+wait
+echo "Converted to PCAP!"
+
+# Now run the plotter:
+pushd /root/jcw78/process_pcap_traces/
+python arrival_time_difference_plot.py "$3/different_cards_run_${run}_card_0.csv" "$3/different_cards_run_${run}_card_1.csv"
+# Move the produced graph:
+mkdir -p $2/graphs/dual_hpt_timestamp_differences
+mv different_cards_run_${run}_card_0.csv_diff_different_cards_run_${run}_card_1.csv.eps $2/graphs/dual_hpt_timestamp_differences/${run}.eps
+popd
+echo "Done... Cleaning up!"
+
+# Delete the extracted expcap files:
+rm -f $3/different_cards_run_${run}_card_0.pcap
+rm -f $3/different_cards_run_${run}_card_1.pcap
+rm -f $3/different_cards_run_${run}_card_0.csv
+rm -f $3/different_cards_run_${run}_card_1.csv
+rm -f $3/different_cards_run_${run}_card_0.expcap.bz
+rm -f $3/different_cards_run_${run}_card_1.expcap.bz
+rm -f $3/different_cards_run_${run}_card_0.expcap
+rm -f $3/different_cards_run_${run}_card_1.expcap
+echo "Done!"
