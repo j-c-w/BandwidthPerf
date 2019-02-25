fd5f5dc285ccb65ccc52ce9bb7cec07e10cefe56
root <root@nf-server12.nf.cl.cam.ac.uk>
2019-02-25 17:09:54 +0000

    Maste clock now syncs to host clock first

--- hpt/clock_sync_master.sh
+++ hpt/clock_sync_master.sh
@@ -5,7 +5,13 @@ if [[ $# -ne 1 ]]; then
 	exit 1
 fi
 
-pkill exanic-clock-sy
+# Make sure that NTP has started.
+/root/jcw78/scripts/hpt/ntp_enable.sh
+
+# And then sync to the host.
+/root/jcw78/scripts/hpt/clock_sync_to_host.sh $1
+
+pkill exanic-clock-sy || echo "Clock sync not running already"
 
 pushd /root/jcw78/scripts/hpt_setup/exanic-software/util/
 ./exanic-config $1 pps-out on
