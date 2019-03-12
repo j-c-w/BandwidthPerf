6747a0476a3f97971905cd185e4c838082d73fb2
root <root@nf-server13.nf.cl.cam.ac.uk>
2019-03-12 16:20:50 +0000

    Add a check to ensure that the CPU flags make sense

--- hpt/record_port.sh
+++ hpt/record_port.sh
@@ -9,6 +9,19 @@ if [[ $# -ne 4 ]] && [[ $# -ne 5 ]]; then
 	exit 1
 fi
 
+cpus_check() {
+	# This function makes a rough check that the CPUs list
+	# is right.  I ran into a lot of head-scratching bugs
+	# due to issues with this list I didn't relize.  This
+	# isn't a full check!
+	local cpus_arg=$1
+
+	if [[ $(tr -dc ':' <<<"$cpus_arg" | awk '{ print length; }')  != 2 ]]; then
+		echo "Error: require a format suitable to pass to the HPT --cpus flag.  Instead got: $cpus_arg"
+		exit 1
+	fi
+}
+
 # Make sure that the port is enabled first.
 pushd /root/jcw78/scripts/hpt_setup/exanic-software/util
 ./exanic-config $1 up
@@ -24,6 +37,8 @@ if [[ $# -eq 4 ]]; then
 	cpus=$3
 	cmd_out_file=$4
 
+	cpus_check $cpus
+
 	# Make sure that the appropriate folders exist:
 	mkdir -p $(dirname $out_file)
 	mkdir -p $(dirname $cmd_out_file)
@@ -38,6 +53,8 @@ if [[ $# -eq 5 ]]; then
 	out_file=$3
 	cpus=$4
 	cmd_out_file=$5
+
+	cpus_check $cpus
 
 	# Make sure that the appropriate folders exist:
 	mkdir -p $(dirname $out_file)
