c9730938d5d65756028d3917103293b80c7772fe
root <root@nf-server09.nf.cl.cam.ac.uk>
2019-03-13 09:17:23 +0000

    Add script to install solarflare drivers

---
+++ setup/install_sf_drivers.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+if [[ $# -ne 1 ]]; then
+	echo "Usage $0 <SF driver zip>"
+	exit 1
+fi
+unzip $1 -d slf_drivers
+# Go to the extract zip file.
+cd slf_drivers
+
+# Convert to DPKG
+alien -c *.noarch.rpm
+
+# And install
+dpkg -i *.deb
