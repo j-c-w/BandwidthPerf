0a10edb11b35601ef05b4854b02083db7ef786c3
root <root@nf-server13.nf.cl.cam.ac.uk>
2019-05-02 21:20:09 +0100

    Add flags to optionally disable capture

--- apps/capture_run.sh
+++ apps/capture_run.sh
@@ -1,5 +1,6 @@
 #!/bin/zsh
 
+set -x
 set -eu
 zmodload zsh/mapfile
 
@@ -13,7 +14,8 @@ source /root/jcw78/scripts/general/parse_config.sh
 source /root/jcw78/scripts/general/remote_run.sh
 
 typeset -a dry_run
-zparseopts -D -E -dry-run=dry_run
+typeset -a no_capture
+zparseopts -D -E -dry-run=dry_run -no-capture=no_capture
 
 runs=$(get_config_value runs)
 lts_directory=$(get_config_value LTSLocation)
@@ -71,24 +73,26 @@ for run in $(seq 1 $runs); do
 	for machine in ${machines[@]}; do
 		remote_run_command $machine "rm -rf $results_directory"
 	done
-	# Start all the capture cards:
-	for machine in ${capture_machines[@]}; do
-		# Kill any ongoing recording going on:
-		remote_run_script $machine hpt/stop_recording.sh
-		# Get the interfaces/CPUs we are using on those machines.
-		use_both=$(get_config_value "${machine}_both_ports" /root/jcw78/scripts/apps/capture_config)
-		# TODO -- IT WOULD BE GOOD TO AUTO-MOUNT THE CAPTURE LOCAITON.
-		cpus=$(get_config_value "${machine}_cpus" /root/jcw78/scripts/apps/capture_config)
-		capture_location=$(get_config_value "${machine}_capture_location" /root/jcw78/scripts/apps/capture_config)
-		if [[ $use_both == *Yes* ]]; then
-			interface1=$(get_config_value "${machine}_if1" /root/jcw78/scripts/apps/capture_config)
-			interface2=$(get_config_value "${machine}_if2" /root/jcw78/scripts/apps/capture_config)
-			remote_run_script $machine hpt/record_port.sh $interface1 $interface2 $capture_location/$benchmark/${num_machines}_machines/$machine $cpus $capture_location/$benchmark/${num_machines}_machines/${machine}_cmd_out
-		else
-			interface1=$(get_config_value "${machine}_if1" /root/jcw78/scripts/apps/capture_config)
-			remote_run_script $machine hpt/record_port.sh $interface1 $capture_location/$benchmark/${num_machines}_machines/$machine $cpus $capture_location/$benchmark/${num_machines}_machines/${machine}_cmd_out
-		fi
-	done
+	if [[ ${#no_capture} == 0 ]]; then
+		# Start all the capture cards:
+		for machine in ${capture_machines[@]}; do
+			# Kill any ongoing recording going on:
+			remote_run_script $machine hpt/stop_recording.sh
+			# Get the interfaces/CPUs we are using on those machines.
+			use_both=$(get_config_value "${machine}_both_ports" /root/jcw78/scripts/apps/capture_config)
+			# TODO -- IT WOULD BE GOOD TO AUTO-MOUNT THE CAPTURE LOCAITON.
+			cpus=$(get_config_value "${machine}_cpus" /root/jcw78/scripts/apps/capture_config)
+			capture_location=$(get_config_value "${machine}_capture_location" /root/jcw78/scripts/apps/capture_config)
+			if [[ $use_both == *Yes* ]]; then
+				interface1=$(get_config_value "${machine}_if1" /root/jcw78/scripts/apps/capture_config)
+				interface2=$(get_config_value "${machine}_if2" /root/jcw78/scripts/apps/capture_config)
+				remote_run_script $machine hpt/record_port.sh $interface1 $interface2 $capture_location/$benchmark/${num_machines}_machines/$machine $cpus $capture_location/$benchmark/${num_machines}_machines/${machine}_cmd_out
+			else
+				interface1=$(get_config_value "${machine}_if1" /root/jcw78/scripts/apps/capture_config)
+				remote_run_script $machine hpt/record_port.sh $interface1 $capture_location/$benchmark/${num_machines}_machines/$machine $cpus $capture_location/$benchmark/${num_machines}_machines/${machine}_cmd_out
+			fi
+		done
+	fi
 	echo "Old results cleared... Starting  new run"
 	for benchmark in "${benchmarks[@]}"; do
 		./run.sh start $benchmark
@@ -110,31 +114,46 @@ for run in $(seq 1 $runs); do
 
 	sleep 3
 
-	# Stop the capture
-	for machine in ${capture_machines[@]}; do
-		remote_run_script $machine hpt/stop_recording.sh
-	done
+	if [[ ${#no_capture} -eq 0 ]]; then
+		# Stop the capture
+		for machine in ${capture_machines[@]}; do
+			remote_run_script $machine hpt/stop_recording.sh
+		done
+	fi
 
 	# Get the files from each machine.  If they don't exist,
 	# that's alright, the machines just might not have been
 	# involved.
+	# First clear any old results from the folder we are about
+	# to populate.
+	if [[ -d $lts_directory/apps_capture/$label/${num_machines}_machines/run/run_$run ]]; then
+		rm -rf $lts_directory/apps_capture/$label/${num_machines}_machines/run/run_$run
+	fi
 	mkdir -p $lts_directory/apps_capture/$label/${num_machines}_machines/run/run_$run
-	for machine in ${machines[@]}; do
+	for machine in ${machines[@]:0:$num_machines}; do
+		# We need to make sure that the directory is created
+		# regardless of whether we copy anything back.
+		mkdir -p $lts_directory/apps_capture/$label/${num_machines}_machines/run/run_$run/$machine
 		scp -r $machine:$results_directory $lts_directory/apps_capture/$label/${num_machines}_machines/run/run_$run/$machine || echo "It seems that machine $machine did not produce any data."
 	done
 
-	# Compress the capture files from each machine, then put them into the LTS:
-	for machine in ${capture_machines[@]}; do
-		remote_run_command $machine "mkdir -p $lts_directory/apps_capture/$label/${num_machines}_machines/run/run_$run"
-		remote_run_command $machine "bzip2 $capture_location/$label/${num_machines}_machines/${machine}-0.expcap; mv $capture_location/$label/${num_machines}_machines/${machine}-0.expcap.bz2 $lts_directory/apps_capture/$label/${num_machines}_machines/run/run_$run/${machine}.expcap.bz2; mv $capture_location/$label/${num_machines}_machines/${machine}_cmd_out $lts_directory/apps_capture/$label/${num_machines}_machines/run/run_$run/${machine}_cmd_out" &
-	done
-	wait
+	if [[ ${#no_capture} == 0 ]]; then
+		# Compress the capture files from each machine, then put them into the LTS:
+		for machine in ${capture_machines[@]}; do
+			remote_run_command $machine "mkdir -p $lts_directory/apps_capture/$label/${num_machines}_machines/run/run_$run"
+			remote_run_command $machine "bzip2 $capture_location/$label/${num_machines}_machines/${machine}-0.expcap; mv $capture_location/$label/${num_machines}_machines/${machine}-0.expcap.bz2 $lts_directory/apps_capture/$label/${num_machines}_machines/run/run_$run/${machine}.expcap.bz2; mv $capture_location/$label/${num_machines}_machines/${machine}_cmd_out $lts_directory/apps_capture/$label/${num_machines}_machines/run/run_$run/${machine}_cmd_out" &
+		done
+		wait
+	fi
 
 	# Also get the log information and the host information.
-	for machine in ${machines[@]}; do
+	for machine in ${machines[@]:0:$num_machines}; do
 		echo "Getting log files from $machine"
-		scp $machine:~/hostinfo $lts_directory/apps_capture/$label/${num_machines}_machines/run/run_$run/$machine || echo "Machine $machine did not dump host information"
+		scp $machine:~/hostinfo $lts_directory/apps_capture/$label/${num_machines}_machines/run/run_$run/$machine
 		scp -r $machine:~/logs $lts_directory/apps_capture/$label/${num_machines}_machines/run/run_$run/$machine
 	done
+
+	echo "Done with run!"
 done
+echo "Done with capture!"
 popd
--- apps/run_all.sh
+++ apps/run_all.sh
@@ -4,8 +4,20 @@ echo "This script runs and captures all of the setups defined"
 echo "in the ./apps file.  See that file for comments on how"
 echo "to modify it"
 
+set -x
+set -eu
+typeset -a dry_run
+typeset -a no_capture
+zparseopts -D -E -dry-run=dry_run -no-capture=no_capture
+
+typeset -a lines
+
 # Parse the apps file.  
 while IFS= read -r line; do
+	lines+="$line"
+done < apps
+
+for line in ${lines[@]}; do
 	if [[ $line == *#* ]] || [[ $line == '' ]]; then
 		continue
 	fi
@@ -15,6 +27,15 @@ while IFS= read -r line; do
 	number_of_machines=$(cut -f2 -d ' ' <<< $line)
 	# All remaining items are benchmarks.
 	benchmarks=$(cut --complement -f1,2 -d' ' <<< $line)
+	if [[ ${#dry_run} == 1 ]]; then
+		echo "Name $name, number $number_of_machines, benchmarks $benchmarks"
+		continue
+	fi
 
-	./capture_run.sh $benchmarks $number_of_machines $name --dry-run
-done < apps
+	if [[ ${#no_capture} == 0 ]]; then
+		./capture_run.sh $benchmarks $number_of_machines $name
+	else
+		./capture_run.sh $benchmarks $number_of_machines $name --no-capture
+	fi
+	echo "Run finished, moving on to next run"
+done
