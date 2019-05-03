bdd794b505f23e6b4fb85973dae76f7189e40252
Jackson Woodruff <woodruff.jackson@gmail.com>
2019-05-03 15:52:11 +0100

    If recording is not set, do not attempt to set up the HPT machine

--- evaluation/nrg/rate/setup_nrg_machines.sh
+++ evaluation/nrg/rate/setup_nrg_machines.sh
@@ -22,6 +22,8 @@ machD=$(get_config_value "MachineD")
 NICA=$(get_config_value "NICA")
 NICD=$(get_config_value "NICD")
 
+use_hpt=$(get_config_value "Record")
+
 nvme_dev_name=$(get_config_value "NVMeDeviceName")
 echo "Config values extracted for setup.  Starting running on"
 echo $machA
@@ -35,8 +37,10 @@ remote_run $machB ../../../setup/pre_setup_machine.sh
 remote_run $machC ../../../setup/pre_setup_machine.sh
 remote_run $machD ../../../setup/pre_setup_machine.sh
 
-# Run the exanic setup on machine C.
-remote_run_script $machC setup/setup_exanic_machine.sh
+if [[ $use_hpt == True ]]; then
+	# Run the exanic setup on machine C.
+	remote_run_script $machC setup/setup_exanic_machine.sh
+fi
 
 # Now, flash OSNT to the NetFPGA in machine B.
 remote_run_script $machB bitfiles/setup_bitfile.sh super_gadget.bit
@@ -45,5 +49,7 @@ remote_run_script $machB bitfiles/setup_bitfile.sh super_gadget.bit
 remote_run_command $machA "ifconfig $NICA up 192.168.0.2"
 remote_run_command $machD "ifconfig $NICD up 192.168.0.3"
 
-# Make sure the long term storage and the temporary NVMe are there on the capturing device.
-remote_run_script $machC setup/setup_nvme.sh $nvme_dev_name
+if [[ $use_hpt == True ]]; then
+	# Make sure the long term storage and the temporary NVMe are there on the capturing device.
+	remote_run_script $machC setup/setup_nvme.sh $nvme_dev_name
+fi
