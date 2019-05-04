93c4ce582716e42f308a2fc1e93379be186afb59
Jackson Woodruff <woodruff.jackson@gmail.com>
2019-05-04 21:38:52 +0100

    Fixed reboot machines to work from any directory

--- apps/reboot_machines.sh
+++ apps/reboot_machines.sh
@@ -3,6 +3,12 @@
 set -eu
 source /root/jcw78/scripts/general/remote_run.sh
 
-for machine in $(cat < machines); do
-	remote_run_command $machine "reboot"
-done
+if [[ $# -eq 0 ]]; then
+	for machine in $(cat < machines); do
+		remote_run_command $machine "reboot"
+	done
+else
+	for machine in $@; do
+		remote_run_command $machine "reboot"
+	done
+fi
